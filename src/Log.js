6c04ac6b05402efae878b3e484bab093e6f29ebc
jason <jasonwchristiansen@gmail.com>
2024-07-10 23:56:56 -0500

    remove extra, restructure

--- src/Log.js
+++
@@ -1,7 +0,0 @@
-const Log = {
-  info: (msg) => Log.log("info", msg),
-  error: (msg) => Log.log("error", msg),
-  log: (type, msg) => console.log(`[${type}]: ${typeof msg == "object" ? JSON.stringify(msg) : msg}`),
-}
-
-export { Log };
